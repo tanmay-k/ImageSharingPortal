ca2a6c7ec612835c01c68e597ba43ddfd7235705
Tanmay Kulkarni <tanmaypkulkarni@gmail.com>
2019-02-26 17:56:11 +0530

    Update

--- modules/main.js
+++ modules/main.js
@@ -4,7 +4,7 @@ class createSite extends HTMLElement {
 		var shadow = this.attachShadow({mode:'open'})
 
 		shadow.innerHTML = `
-		<link rel=stylesheet href="../css/contents.css">
+		<link rel=stylesheet href="../css/main.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
@@ -32,12 +32,20 @@ class createSite extends HTMLElement {
 			<div class="col-sm-6">
 				<div class="jumbotron">
 					<h1>Create Profile</h1>
+  						<div class="media p-3">
+							<div class="tooltip">Hello Hello<a href="#"><img src="avatar.png" alt="" class="mr-3 rounded-circle" style="width:60px;"></a>
+								<span class="tooltiptext">Update Profile Photo</span>
+							</div>
+    							<!--div class="media-body">
+      								<button type="button" id="updateProfilePhotoBtn" class="btn btn-sm btn-primary mt-2" onclick="process()">Update Profile Photo</button>
+    							</div-->
+  						</div>
 					<form class="form" enctype="multipart/formdata">
 						<label for="user-name" class="mr-sm-2">Your Name:</label>
 						<input type="text" class="form-control mb-2 mr-sm-2" id="user-name">
 						<label for="bio" class="mr-sm-2">Bio (optional) :</label>
 						<textarea id="bio" class="form-control mb-2 mr-sm-2"></textarea>
-						<button type="button" id="createSiteBtn" class="btn btn-primary" onclick="process()">Create Site</button>
+						<button type="button" id="createSiteBtn" class="btn btn-primary float-right" onclick="process()">Create Site</button>
 					</form>
 				</div>
 			</div>
