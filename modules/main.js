7680b3aceabfb1403f61ded61738b357380df7f5
Tanmay Kulkarni <tanmaypkulkarni@gmail.com>
2019-02-26 23:13:30 +0530

    Update

--- modules/main.js
+++ modules/main.js
@@ -2,7 +2,7 @@ class createSite extends HTMLElement {
 	constructor() {
 		super()
 		var shadow = this.attachShadow({mode:'open'})
-
+		var avatarSrc = "avatar.png"
 		shadow.innerHTML = `
 		<link rel=stylesheet href="../css/main.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
@@ -33,19 +33,18 @@ class createSite extends HTMLElement {
 				<div class="jumbotron">
 					<h1>Create Profile</h1>
   						<div class="media p-3">
-							<div class="tooltip">Hello Hello<a href="#"><img src="avatar.png" alt="" class="mr-3 rounded-circle" style="width:60px;"></a>
-								<span class="tooltiptext">Update Profile Photo</span>
-							</div>
-    							<!--div class="media-body">
-      								<button type="button" id="updateProfilePhotoBtn" class="btn btn-sm btn-primary mt-2" onclick="process()">Update Profile Photo</button>
-    							</div-->
+							<label for="avatar">
+								<img id="avatar-img" src="${avatarSrc}" alt="" class="mr-3 rounded-circle" style="width:60px;" data-toggle="tooltip" title="Upload your photo">
+							</label>
+							<input type="file" style="visibility:hidden" id="avatar" name="user-avatar" accept="image/*" onchange="uploadProfilePhoto(this.files,'abcd')">
   						</div>
 					<form class="form" enctype="multipart/formdata">
 						<label for="user-name" class="mr-sm-2">Your Name:</label>
 						<input type="text" class="form-control mb-2 mr-sm-2" id="user-name">
 						<label for="bio" class="mr-sm-2">Bio (optional) :</label>
 						<textarea id="bio" class="form-control mb-2 mr-sm-2"></textarea>
-						<button type="button" id="createSiteBtn" class="btn btn-primary float-right" onclick="process()">Create Site</button>
+						<button type="button" id="createSiteBtn" class="btn btn-primary float-right" onclick="process(${shadow})">Create Site</button>
+
 					</form>
 				</div>
 			</div>
