f6dc5f02a7b5bcf64625626006e40ded07728eb3
tanmay-k <47519041+tanmay-k@users.noreply.github.com>
2019-02-26 15:58:22 +0530

    Update createUserSite.js

--- scripts/createUserSite.js
+++ scripts/createUserSite.js
@@ -1,1 +1,5 @@
 //This script will fork main site to create user's personal website 
+function process()	{ //Just sample method
+	console.log("You clicked on create site button!")
+	alert("You clicked on create site button!")
+}
